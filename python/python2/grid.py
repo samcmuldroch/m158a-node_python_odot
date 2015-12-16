015a968dfb4daf5b9cd5c853f7d959a617b9696b
Sydney McMuldroch <samcmuldroch@berkeley.edu>
2015-12-15 19:30:21 -0800

    Current grid

--- python/python2/grid.py
+++ python/python2/grid.py
@@ -1,16 +1,7 @@
-"""
- Example program to show using an array to back a grid on-screen.
- 
- Sample Python/Pygame Programs
- Simpson College Computer Science
- http://programarcadegames.com/
- http://simpson.edu/computer-science/
- 
- Explanation video: http://youtu.be/mdTeqiWyFnc
-"""
 import pygame
 from pygame.locals import *
-
+from pygame.compat import unichr_, unicode_
+import locale
 import os
 import sys
 import re
@@ -23,13 +14,152 @@ from bs4 import BeautifulSoup
 from OSC import OSCClient, OSCBundle
 from PIL import Image
 
-pygame.init()
-
+#FIX THE GOING UP TO THE NEXT OCTAVE
 #error noise should be based on the frequency
 #implement combo
 #playing multiple notes at same time (need to play each note quieter)
 #figure-out notes for a song
 #possibly have 3 differen't song options
+#have actual song playing in background which gets quieter/louder depending on current score
+
+
+print("Enter the Song you would like to play.")
+print("1--Easy Song")
+print("2--Medium Song")
+print("3--Difficult Song")
+whichKeyboard = "False"
+while True:
+    whichKeyboard = raw_input("Enter the key corresponding to which song you want to play (NONE if you would like quit): ")
+    if (whichKeyboard == "NONE"):
+        break;
+    if (whichKeyboard == "1"):
+        break;
+    if (whichKeyboard == "2"):
+        break;
+    if (whichKeyboard == "3"):
+        break;
+
+combo = 0
+maxCombo = 0
+
+
+
+class cFontManager:
+    '''
+    A simple class used to manage Font objects and provide a simple way to use
+    them to draw text on any surface.
+ 
+    Directly import this file to use the class, or run this file from the
+    command line to see a trivial sample.
+ 
+    Written by Scott O. Nelson  
+    '''
+    def __init__(self, listOfFontNamesAndSizesAsTuple):
+        '''
+        Pass in a tuple of 2-item tuples.  Each 2-item tuple is a fontname / 
+        size pair. To use the default font, pass in a None for the font name.
+        Font objects are created for each of the pairs and can then be used
+        to draw text with the Draw() method below.
+        
+        Ex: fontMgr = cFontManager(((None, 24), ('arial', 18), ('arial', 24),
+            ('courier', 12), ('papyrus', 50)))
+ 
+        TODO: add support for bold & italics
+        '''
+        self._fontDict = {}
+        for pair in listOfFontNamesAndSizesAsTuple:
+            assert len(pair) == 2, \
+                "Pair must be composed of a font name and a size - ('arial', 24)"
+            if pair[0]:
+                fontFullFileName = pygame.font.match_font(pair[0])
+            else:
+                fontFullFileName = None # use default font
+            self._fontDict[pair] = pygame.font.Font(fontFullFileName, pair[1])
+ 
+    def Draw(self, surface, fontName, size, text, rectOrPosToDrawTo, color,
+            alignHoriz='left', alignVert='top', antialias=False):
+        '''
+        Draw text with the given parameters on the given surface.
+        
+        surface - Surface to draw the text onto.
+        
+        fontName - Font name that identifies what font to use to draw the text.
+        This font name must have been specified in the cFontManager 
+        
+        rectOrPosToDrawTo - Where to render the text at.  This can be a 2
+        item tuple or a Rect.  If a position tuple is used, the align
+        arguments will be ignored.
+        
+        color - Color to draw the text with.
+        
+        alignHoriz - Specifies horizontal alignment of the text in the
+        rectOrPosToDrawTo Rect.  If rectOrPosToDrawTo is not a Rect, the
+        alignment is ignored.
+        
+        alignVert - Specifies vertical alignment of the text in the
+        rectOrPosToDrawTo Rect.  If rectOrPosToDrawTo is not a Rect, the
+        alignment is ignored.
+ 
+        antialias - Whether to draw the text anti-aliased or not.
+        '''
+        pair = (fontName, size)
+        assert pair in self._fontDict, \
+            'Font: %s Size: %d is not available in cFontManager.' % pair
+        fontSurface = self._fontDict[(fontName, size)].render(text,
+            antialias, color)
+        if isinstance(rectOrPosToDrawTo, tuple):
+            surface.blit(fontSurface, rectOrPosToDrawTo)
+        elif isinstance(rectOrPosToDrawTo, pygame.Rect):
+            fontRect = fontSurface.get_rect()
+            # align horiz
+            if alignHoriz == 'center':
+                fontRect.centerx = rectOrPosToDrawTo.centerx
+            elif alignHoriz == 'right':
+                fontRect.right = rectOrPosToDrawTo.right
+            else:
+                fontRect.x = rectOrPosToDrawTo.x  # left
+            # align vert
+            if alignVert == 'center':
+                fontRect.centery = rectOrPosToDrawTo.centery
+            elif alignVert == 'bottom':
+                fontRect.bottom = rectOrPosToDrawTo.bottom
+            else:
+                fontRect.y = rectOrPosToDrawTo.y  # top
+                
+            surface.blit(fontSurface, fontRect)
+ 
+def DrawRect(color, key, dimOne, dimTwo, dimThree, dimFour):
+    '''A simple demo of the use of the cFontManager class'''
+    screen = pygame.display.get_surface()
+    fontMgr = cFontManager(((None, 24), (None, 48), ('arial', 24)))
+    rect = pygame.Rect(dimOne, dimTwo, dimThree, dimFour)
+    pygame.draw.rect(screen, color, rect) 
+    rect2 = pygame.Rect(dimOne, dimTwo-4, dimThree, dimFour)       
+    fontMgr.Draw(screen, 'arial', 24, key, rect2, BLACK,
+        'center', 'top')
+
+
+
+pygame.init()
+
+resolution = 400, 200
+screen = pygame.display.set_mode(resolution)
+
+fg = 250, 240, 230
+bg = 5, 5, 5
+wincolor = 40, 40, 90
+
+#fill background
+screen.fill(wincolor)
+
+#load font, prepare values
+font = pygame.font.Font(None, 80)
+text = 'Fonty'
+size = font.size(text)
+
+ren = font.render(text, 0, fg, bg)
+screen.blit(ren, (10, 10))
+
 
 octave = 1.0
 pianoClient1 = OSCClient()
@@ -57,17 +187,27 @@ initialDingClient.send(initialDing)
 
 keyboardToNoteDictionary = {'w' : 'C#/Db', 'e' : 'D#/Eb', 'u' : 'F#/Gb', 'i' : 'G#/Ab', 'o' : 'A#/Bb', 'a' : 'C', 's' : 'D', 'd' : 'E', 'f' : 'F', 'k' : 'G', 'l' : 'A', ';' : 'B', '1' : 'Octave Up', '2' : 'Octave Down'}
 keyboardToFrequencyDictionary = {'w' : 277.18, 'e' : 311.13, 'u' : 369.99, 'i' : 415.30, 'o' : 466.16, 'a' : 261.63, 's' : 293.66, 'd' : 329.63, 'f' : 349.23, 'k' : 392.00, 'l' : 440.00, ';' : 493.88}
-numberToNoteDictionary = {1 : 'a', 2 : 'w', 3 : 's', 4 : 'e', 5 : 'd', 6 : 'f', 7 : 'u', 8 : 'i', 9 : 'o', 10 : 'k', 11 : 'l', 12 : ';'}
+numberToNoteDictionary = {1 : 'a', 2 : 'w', 3 : 's', 4 : 'e', 5 : 'd', 6 : 'f', 7 : 'u', 8 : 'i', 9 : 'o', 10 : 'k', 11 : 'l', 12 : ';', 13 : '1', 14 : '2'}
+keyNumberToNumberDictionary = {97 : 1, 119 : 2, 115 : 3, 101 : 4, 100 : 5, 102 : 6, 117 : 7, 105 : 8, 111 : 9, 107 : 10, 108 : 11, 59 : 12, 49 : 13, 50 : 14}
 
 print(keyboardToNoteDictionary)
 
-def playNote(key, row, noteindex, good, length):
+def playNote(key, row, noteindex, good, length, combo):
+    #print(combo)
+    combo += 1
+    print("Current Combo:"),
+    print(combo)
+    if key == 13 or key == 14:
+        return combo
     note = numberToNoteDictionary[key]
     frequency = keyboardToFrequencyDictionary[note]
     currentNote = OSCBundle()
     #USE LENGTH TO DETERMINE AMPLITUDE SO IT DOESNT SOUND SHITTY PLAYING ALL THE NOTES AT ONCE
     #print(row)
-    envelopeList[0] = 1/(10-row)
+    if row == 8 or row == 9:
+        envelopeList[0] = 1
+    else:
+        envelopeList[0] = 0.5 + 0.5*(row/8)
     if good:
         currentNote.append({'addr': "/frequency", 'args':[frequency * octave]})
         currentNote.append({'addr': "/envelope/line", 'args': envelopeList})
@@ -84,14 +224,15 @@ def playNote(key, row, noteindex, good, length):
         pianoClient4.send(currentNote)
     if noteindex == 5:
         pianoClient5.send(currentNote)
+    return combo
 
 def playError():
+    print("Oops!")
+    combo = 0
     errorClient = OSCClient()
     errorClient.connect(("localhost", 54200))
     error = OSCBundle()
-    error.append({'addr': "/amplitude", 'args':[1]})
-    durationOfError = 1000
-    error.append({'addr': "/startValue", 'args': ['start', durationOfError]})
+    error.append({'addr': "/startValue", 'args': ['start']})
     errorClient.send(error)
 
 
@@ -111,6 +252,8 @@ PINK = (255, 153, 255)
 HOTPINK = (255, 153, 204)
 RED = (255, 153, 153)
 ORANGE = (255, 204, 153)
+LIGHTYELLOW = (255, 255, 204)
+LIGHTGREEN = (229, 255, 204)
 
 
 
@@ -129,36 +272,100 @@ for row in range(10):
     # Add an empty array that will hold each cell
     # in this row
     grid.append([])
-    for column in range(12):
+    for column in range(14):
         grid[row].append(0)  # Append a cell
  
 # Set row 1, cell 5 to one. (Remember rows and
 # column numbers start at zero.)
 
 
-musicGrid = [
-    [1,0,0,4,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [1,0,0,4,0,0,0,0,0,10,11,12],
-    [0,0,3,0,0,0,0,0,0,0,0,0],
-    [0,0,0,4,0,0,0,0,0,0,0,0],
-    [0,0,0,0,5,0,0,0,0,0,0,0],
-    [0,0,0,4,0,0,0,0,0,0,11,0],
-    [0,0,3,0,0,0,0,0,0,0,0,0],
-    [1,0,0,4,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,6,7,0,0,0,0,0],
-    [1,1,2,3,0,0,0,0,0,0,0,0],
-    [1,1,2,3,0,0,0,0,0,0,0,12],
-    [1,0,0,3,0,0,0,0,0,0,0,0],
-    [0,0,2,3,0,0,0,0,0,0,0,0],
-    [0,1,2,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [1,0,2,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,8,0,0,0,0],
-    [0,0,0,0,0,0,0,8,0,0,0,0],
-    [1,2,3,4,5,6,7,8,9,10,11,12], #LAST LINE ACTUALLY REACHED
-    [0,0,0,0,0,0,0,8,0,0,0,0]
-]
+
+musicGrid = []
+
+if whichKeyboard == "NONE":
+    musicGrid = [
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    ]
+if whichKeyboard == "1":
+    musicGrid = [
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,11,0,0,0],
+        [0,0,0,0,0,5,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,11,0,13,0],
+        [0,0,0,0,0,0,0,0,0,0,11,0,0,14],
+        [0,0,0,0,0,5,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,11,0,0,13],
+        [0,0,0,0,0,0,0,0,0,0,11,0,0,14],
+        [0,0,0,0,0,0,0,0,0,0,11,0,0,0],
+        [0,0,0,0,0,5,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,6,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,11,0,0,0],
+        [0,0,0,0,0,5,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,6,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    ]
+    maxCombo = 17
+if whichKeyboard == "2":
+    musicGrid = []
+if whichKeyboard == "3":
+    musicGrid = [
+        [1,0,0,4,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+        [1,0,0,4,0,0,0,0,0,10,11,12,0,0],
+        [0,0,3,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,4,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,5,0,0,0,0,0,0,0,0,0],
+        [0,0,0,4,0,0,0,0,0,0,11,0,0,0],
+        [0,0,3,0,0,0,0,0,0,0,0,0,0,0],
+        [1,0,0,4,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,6,7,0,0,0,0,0,0,0],
+        [1,1,2,3,0,0,0,0,0,0,0,0,0,0],
+        [1,1,2,3,0,0,0,0,0,0,0,12,0,0],
+        [1,0,0,3,0,0,0,0,0,0,0,0,0,0],
+        [0,0,2,3,0,0,0,0,0,0,0,0,0,0],
+        [0,1,2,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0,0,0,0,13,14],
+        [1,0,2,0,0,0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,8,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,8,0,0,0,0,0,0],
+        [1,2,3,4,5,6,7,8,9,10,11,12,0,0], #LAST LINE ACTUALLY REACHED
+        [0,0,0,0,0,0,0,8,0,0,0,0,0,0,0,0]
+    ]
 
 musicGridRow = 0
  
@@ -166,7 +373,7 @@ musicGridRow = 0
 pygame.init()
  
 # Set the HEIGHT and WIDTH of the screen
-WINDOW_SIZE = [305, 255]
+WINDOW_SIZE = [355, 255]
 screen = pygame.display.set_mode(WINDOW_SIZE)
  
 # Set title of screen
@@ -177,71 +384,49 @@ done = False
  
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
- 
+
+keysClicked = []
 # -------- Main Program Loop -----------
 while not done:
     for event in pygame.event.get():  # User did something
-        keysClicked = []
+        #keysClicked = []
         if event.type == pygame.QUIT:  # If user clicked close
             done = True  # Flag that we are done so we exit this loop
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            # User clicks the mouse. Get the position
-            pos = pygame.mouse.get_pos()
-            # Change the x/y screen coordinates to grid coordinates
-            column = pos[0] // (WIDTH + MARGIN)
-            row = pos[1] // (HEIGHT + MARGIN)
-            # Set that location to zero
-            grid[row][column] = 1
-            print("Click ", pos, "Grid coordinates: ", row, column)
         elif event.type == pygame.KEYDOWN:
-            keys = pygame.key.get_pressed()
-            if keys[K_a]:
-                keysClicked.append(1)
-            if keys[K_w]:
-                keysClicked.append(2)
-            if keys[K_s]:
-                keysClicked.append(3)
-            if keys[K_e]:
-                keysClicked.append(4)
-            if keys[K_d]:
-                keysClicked.append(5)
-            if keys[K_f]:
-                keysClicked.append(6)
-            if keys[K_u]:
-                keysClicked.append(7)
-            if keys[K_i]:
-                keysClicked.append(8)
-            if keys[K_o]:
-                keysClicked.append(9)
-            if keys[K_k]:
-                keysClicked.append(10)
-            if keys[K_l]:
-                keysClicked.append(11)
-            if keys[K_SEMICOLON]:
-                keysClicked.append(12)
-                #https://www.pygame.org/docs/ref/key.html
+            if event.key in keyNumberToNumberDictionary.keys():
+                kay = keyNumberToNumberDictionary[event.key]
+                if kay == 13:
+                    print("ocatve up")
+                    octave = octave * 2
+                    keysClicked.append(kay)
+                elif kay == 14:
+                    print("octave down")
+                    octave = octave / 2
+                    keysClicked.append(kay)
+                else:
+                    keysClicked.append(kay)
  
     # Set the screen background
-    print(keysClicked)
     screen.fill(BLACK)
     newgrid = []
     for roww in range(10):
         # Add an empty array that will hold each cell
         # in this row
         newgrid.append([])
-        for columnn in range(12):
+        for columnn in range(14):
             newgrid[roww].append(0)  # Append a cell
 
     # Draw the grid
 
     for row in [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]:
-        for column in range(12):
+        for column in range(14):
             color = WHITE
+            keyy = ''
             newKeysClicked = []
             keyIndex = 1
             for key in keysClicked:
                 if grid[row][column] == key:
-                    playNote(key, row, keyIndex, True, len(keysClicked))
+                    combo = playNote(key, row, keyIndex, True, len(keysClicked), combo)
                     grid[row][column] = 0
                     keyIndex += 1
                 else:
@@ -249,47 +434,56 @@ while not done:
             keysClicked = newKeysClicked
             if grid[row][column] == 1:
                 color = YELLOW
+                keyy = 'a'
             if grid[row][column] == 2:
                 color = YELLOWGREEN
+                keyy = 'w'
             if grid[row][column] == 3:
                 color = GREEN
+                keyy = 's'
             if grid[row][column] == 4:
                 color = BLUEGREEN
+                keyy = 'e'
             if grid[row][column] == 5:
                 color = BLUE
+                keyy = 'd'
             if grid[row][column] == 6:
                 color = DARKBLUE
+                keyy = 'f'
             if grid[row][column] == 7:
                 color = BLUEPURPLE
+                keyy = 'u'
             if grid[row][column] == 8:
                 color = PURPLE
+                keyy = 'i'
             if grid[row][column] == 9:
                 color = PINK
+                keyy = 'o'
             if grid[row][column] == 10:
                 color = HOTPINK
+                keyy = 'k'
             if grid[row][column] == 11:
                 color = RED
+                keyy = 'l'
             if grid[row][column] == 12:
                 color = ORANGE
+                keyy = ';'
+            if grid[row][column] == 13:
+                color = LIGHTYELLOW
+                keyy = '1'
+            if grid[row][column] == 14:
+                color = LIGHTGREEN
+                keyy = '2'
             if row == 9:
-                #something important about top row
                 row1 = 0
-                if grid[row][column] > 0:
-                    playError()
             else:
                 row1 = row + 1
                 newgrid[row1][column] = grid[row][column]
-                pygame.draw.rect(screen,
-                             color,
-                             [(MARGIN + WIDTH) * column + MARGIN,
-                              (MARGIN + HEIGHT) * row1 + MARGIN,
-                              WIDTH,
-                              HEIGHT])
+                DrawRect(color, keyy, (MARGIN + WIDTH) * column + MARGIN, (MARGIN + HEIGHT) * row1 + MARGIN, WIDTH, HEIGHT)
 
     keyIndexx = 1
     for key in newKeysClicked:
-        playNote(key, -1, keyIndexx, False, len(newKeysClicked))
-        keyIndexx += 1
+        playError()
 
     musicGridRow += 1
     if musicGridRow >= len(musicGrid)-1:
@@ -309,13 +503,14 @@ while not done:
 # Be IDLE friendly. If you forget this line, the program will 'hang'
 # on exit.
 print('APPLAUSE')
+if maxCombo == 0:
+    maxCombo = combo
 #applause based on rating
 endingClient = OSCClient()
 endingClient.connect(("localhost", 54300))
 ending = OSCBundle()
-rating = 10 #RATING SHOULD BE BASED ON COMBO SCORE SHOULD BE NUMBER BETWEEN 1 AND 10
-ending.append({'addr': "/amplitude", 'args':[rating/10]})
-durationOfApplause = ((10 - rating) / 10) * 6000
+ending.append({'addr': "/amplitude", 'args':[combo/maxCombo]})
+durationOfApplause = ((maxCombo - combo) / maxCombo) * 6000
 ending.append({'addr': "/startValue", 'args': ['start', durationOfApplause]})
 endingClient.send(ending)
 
